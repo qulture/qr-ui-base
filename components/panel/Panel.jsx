cd0eaad358133e42a4edd8d7354b1fe6dba7661a
Christian Storch <ccstorch@gmail.com>
2016-04-30 19:47:19 +0100

    Transparent option for Panel component

--- components/panel/Panel.jsx
+++ components/panel/Panel.jsx
@@ -1,14 +1,19 @@
 import React, { PropTypes } from 'react';
 
 function Panel(props) {
-  const {children} = props;
-  const className = 'qr-panel';
+  const { children, transparent } = props;
+  let className = 'qr-panel';
+  if(!!transparent) className += ' transparent';
 
   return (
     <div className={className}>
       {children}
     </div>
   );
+}
+
+Panel.propTypes = {
+  transparent: PropTypes.bool
 }
 
 export default Panel;
