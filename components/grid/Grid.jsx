4f954c99fe74f6f765561b3366663d1b9954b1ab
Christian Storch <ccstorch@gmail.com>
2016-05-01 11:24:33 +0100

    Fixed boxed option for grid

--- components/grid/Grid.jsx
+++ components/grid/Grid.jsx
@@ -1,12 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 
 function Grid(props) {
-  const { direction, fullHeight, align, children, noWrap, justify } = props;
+  const { direction, fullHeight, align, children, noWrap, justify, boxed } = props;
 
   let className = `qr-grid ${direction}-direction ${align}-align justify-${justify}`;
   if(!!fullHeight) className += ' full-height';
   if(!!noWrap) className += ' no-wrap';
-  if(!!noWrap) className += ' boxed';
+  if(!!boxed) className += ' boxed';
 
   return (
     <div className={className}>
