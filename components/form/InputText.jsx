b38f8e9e1185fbc499030a86d9fdc4dd40c845fc
Christian Storch <ccstorch@gmail.com>
2016-05-01 14:14:23 +0100

    Textarea type for InputText and auto resize

--- components/form/InputText.jsx
+++ components/form/InputText.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import { Boxed } from './../boxed';
 import { focusInputByRef } from 'meteor/cinn:react-form-helpers';
 import InputLabel from './InputLabel';
@@ -9,7 +10,11 @@ class InputText extends Component {
   render() {
     const { type, value, placeholder, label, onChange, color, rightContent, leftContent, focus, onlyInput } = this.props;
     const ref = !!focus ? 'focus' : '';
-    const inputDOMItem = <input type={type} ref={ref} value={value} placeholder={placeholder} onChange={onChange} />;
+    const inputProps = { ref, value, placeholder, onChange };
+
+    let inputDOMItem = <input type={type} {...inputProps} />;
+    if(type === 'textarea') inputDOMItem = <TextareaAutosize minRows={5} {...inputProps} />;
+
     const boxedInputDOMItem = <Boxed color={color} rightContent={rightContent} leftContent={leftContent} noPadding>{inputDOMItem}</Boxed>;
 
     if(!!onlyInput) return inputDOMItem;
