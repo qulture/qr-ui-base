c7550bbd343484e4799dc75c5b1818431d1d7436
Christian Storch <ccstorch@gmail.com>
2016-05-05 16:31:07 +0100

    noPaddingLeft for ContainerHeader component

--- components/container/ContainerHeader.jsx
+++ components/container/ContainerHeader.jsx
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react';
 import Icon from './../Icon';
 
 function ContainerHeader(props) {
-  const { children, returnLink, large, fullWidth } = props;
+  const { children, returnLink, large, fullWidth, noPaddingLeft } = props;
   const returnDOMItem = !!returnLink ? <Icon className="qr-container-title-return-link" color="detail-2" size="xx-large" type="angle-left" link={returnLink} /> : '';
   let className = 'qr-container-title';
   if(!!fullWidth) className += ' full-width';
   if(!!large) className += ' large';
+  if(!!noPaddingLeft) className += ' no-padding-left';
 
   return (
     <div className={className}>
@@ -20,6 +21,7 @@ ContainerHeader.propTypes = {
   returnLink: PropTypes.string,
   large: PropTypes.bool,
   fullWidth: PropTypes.bool,
+  noPaddingLeft: PropTypes.bool,
 }
 
 export default ContainerHeader;
