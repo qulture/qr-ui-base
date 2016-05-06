765788c48fb38afabb0e612805eae7c114c24219
Christian Storch <ccstorch@gmail.com>
2016-05-06 18:31:08 +0100

    Fixed height option for PanelContentList component

--- components/panel/PanelContentList.jsx
+++ components/panel/PanelContentList.jsx
@@ -1,13 +1,20 @@
 import React, { PropTypes } from 'react';
 
 function PanelContentList(props) {
-  const { children } = props;
+  const { children, fixedHeight } = props;
+  let className = 'qr-panel-content-list';
+  if(!!fixedHeight) className += ' fixed-height';
+  console.log(fixedHeight, className);
 
   return (
-    <ul className="qr-panel-content-list">
+    <ul className={className}>
       {children}
     </ul>
   );
 }
+
+PanelContentList.propTypes = {
+  fixedHeight: PropTypes.bool
+};
 
 export default PanelContentList;
