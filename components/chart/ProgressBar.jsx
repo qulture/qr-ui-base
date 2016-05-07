fb1e055c32f331728fa4a7842c8f4c1e78150dc2
Christian Storch <ccstorch@gmail.com>
2016-05-07 17:07:04 +0100

    ProgressBar component

---
+++ components/chart/ProgressBar.jsx
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+
+function ProgressBar(props) {
+  const { min, max, value, sufix, prefix } = props;
+  const lineWidth = (value / (max - min)) * 100;
+  const lineStyle = { width: `${lineWidth}%` };
+
+  return (
+    <div className="qr-progress-bar-wrapper">
+      <div className="qr-progress-bar-label detail-3-color">
+        {prefix}
+        {value}
+        {sufix}
+      </div>
+      <div className="qr-progress-bar">
+        <div className="qr-progress-bar-line detail-4-bg-color" style={lineStyle}></div>
+      </div>
+    </div>
+  );
+}
+
+ProgressBar.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  value: PropTypes.number,
+  sufix: PropTypes.string,
+  prefix: PropTypes.string
+};
+
+
+ProgressBar.defaultProps = {
+  min: 0,
+  max: 100,
+};
+
+export default ProgressBar;
--- components/chart/index.js
+++ components/chart/index.js
@@ -1,9 +1,11 @@
 import LineChart from './LineChart';
 import BarChart from './BarChart';
 import ProgressChart from './ProgressChart';
+import ProgressBar from './ProgressBar';
 
 export {
   LineChart,
   BarChart,
-  ProgressChart
+  ProgressChart,
+  ProgressBar
 };
