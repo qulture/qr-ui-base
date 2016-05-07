c1bc5328397801d8cbbe8cc5bebdcc870cfec595
Christian Storch <ccstorch@gmail.com>
2016-05-07 20:25:50 +0100

    GradesBar component

---
+++ components/chart/GradesBar.jsx
@@ -0,0 +1,46 @@
+import React, { PropTypes, Component } from 'react';
+import { SemaphoreColors } from 'meteor/qulture:qr-ui-base/helpers';
+
+class GradesBar extends Component {
+  renderValues() {
+    const { max, min, values, valueKey } = this.props;
+
+    const valuesDOMItems = _.map(values, function(item) {
+      const value = item[valueKey];
+      const itemWidth = value * 10;
+      const style = { width: `${itemWidth}` };
+      const semaphoreColor = SemaphoreColors.getSemaphoreColor(value, { max, min });
+      const className = `qr-grades-bar-item grading-${semaphoreColor}-bg-color`;
+      return <div className={className} style={style}></div>
+    });
+
+    return valuesDOMItems;
+  }
+
+  render() {
+    const valuesDOMItems = this.renderValues();
+
+    return (
+      <div className="qr-grades-bar">
+        {valuesDOMItems}
+      </div>
+    );
+  }
+}
+
+GradesBar.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
+  values: PropTypes.array,
+  labelKey: PropTypes.string,
+  valueKey: PropTypes.string
+};
+
+GradesBar.defaultProps = {
+  max: 5,
+  min: 1,
+  labelKey: 'name',
+  valueKey: 'value'
+};
+
+export default GradesBar;
--- components/chart/index.js
+++ components/chart/index.js
@@ -2,10 +2,12 @@ import LineChart from './LineChart';
 import BarChart from './BarChart';
 import ProgressChart from './ProgressChart';
 import ProgressBar from './ProgressBar';
+import GradesBar from './GradesBar';
 
 export {
   LineChart,
   BarChart,
   ProgressChart,
-  ProgressBar
+  ProgressBar,
+  GradesBar
 };
