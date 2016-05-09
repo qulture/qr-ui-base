873e11c57adcc689c1bfac699d5fd6829666257a
Christian Storch <ccstorch@gmail.com>
2016-05-09 16:19:56 +0100

    TableHeaderCell with small option

--- components/table/TableHeaderCell.jsx
+++ components/table/TableHeaderCell.jsx
@@ -2,8 +2,9 @@ import React, { PropTypes } from 'react';
 import Icon from './../Icon';
 
 function TableHeaderCell(props) {
-  let { className, large, icon, iconLink, width } = props;
+  let { className, large, small, icon, iconLink, width } = props;
   if(!!large) className += ' large';
+  if(!!small) className += ' small';
   const style = { width: `${width}%` };
 
   const iconDOMItem = !!icon ? <Icon type={icon} size="large" link={iconLink} /> : '';
@@ -21,6 +22,7 @@ TableHeaderCell.propTypes = {
   icon: PropTypes.string,
   iconLink: PropTypes.string,
   large: PropTypes.bool,
+  small: PropTypes.bool,
   width: PropTypes.number
 };
 
