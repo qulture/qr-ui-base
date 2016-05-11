74809dbcb355b4dc1c635f041281e2f4789c84e8
Christian Storch <ccstorch@gmail.com>
2016-05-11 13:06:23 +0100

    ContainerContent with withPadding option

--- components/container/ContainerContent.jsx
+++ components/container/ContainerContent.jsx
@@ -1,12 +1,13 @@
 import React, { PropTypes } from 'react';
 
 function ContainerContent(props) {
-  const { children, color, large, fullWidth, withSidebar, withSmallSidebar, withMediumSidebar } = props;
+  const { children, color, large, fullWidth, withSidebar, withSmallSidebar, withMediumSidebar, withPadding } = props;
   let className = 'qr-container-content';
   if(!!fullWidth) className += ' full-width';
   if(!!withSidebar) className += ' with-sidebar';
   if(!!withSmallSidebar) className += ' with-small-sidebar';
   if(!!withMediumSidebar) className += ' with-medium-sidebar';
+  if(!!withPadding) className += ' with-padding';
   if(!!large) className += ' large';
 
   return (
@@ -23,6 +24,7 @@ ContainerContent.propTypes = {
   withSidebar: PropTypes.bool,
   withSmallSidebar: PropTypes.bool,
   withMediumSidebar: PropTypes.bool,
+  withPadding: PropTypes.bool,
 }
 
 export default ContainerContent;
