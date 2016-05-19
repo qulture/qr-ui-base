b1e968a3f1c011ca5bfe5475c2a0db756eb19a9a
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:01:45 +0100

    Container using custom color

--- components/container/Container.jsx
+++ components/container/Container.jsx
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 
 function Container(props) {
   const { userColor, bgColor, children } = props;
   const className = `qr-container ${userColor}-user ${bgColor}-bg-color`;
+  const style = ColorHelpers.generateStyleForColor('bg', bgColor);
 
   return (
-    <div className={className}>
+    <div className={className} style={style}>
       {children}
     </div>
   )
