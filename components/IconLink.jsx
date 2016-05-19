4832d909ef1d5bbdd8ab643461c56e1af99f5758
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:06:52 +0100

    IconLink using custom color

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -1,14 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+import { ColorHelpers } from './../helpers';
 import Icon from './Icon';
 
 function IconLink(props) {
   const { children, type, size, link, color, action } = props;
   const className = `qr-icon-link ${size} ${color}-color ${props.className}`;
+  const style = ColorHelpers.generateStyleForColor('color', color);
 
   return (
-    <a href={link} onClick={action} className={className}>
+    <a href={link} onClick={action} className={className} style={style}>
       <Icon type={type} size={size} color={color} />
-      <span>{children}</span>
+      <span style={style}>{children}</span>
     </a>
   );
 }
