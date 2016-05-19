deafa458792736cdd059f2e00da6ef0bab4b2f51
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:37:38 +0100

    Table using custom color

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -1,18 +1,21 @@
 import _ from 'lodash';
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 
 function Table(props) {
   const { borderTop, noColumnsPaddingLeft, noColumnsPaddingRight, noColumnsPaddingSides, smallPadding } = props;
   let className = 'qr-table';
   if(!!borderTop) className += ' border-top';
-  if(_.isString(borderTop)) className += ` ${borderTop}-border-top-color`;
+  if(_.isString(borderTop)) className += ` ${borderTop}-border-color`;
   if(!!noColumnsPaddingLeft) className += ' no-columns-padding-left';
   if(!!noColumnsPaddingRight) className += ' no-columns-padding-right';
   if(!!noColumnsPaddingSides) className += ' no-columns-padding-sides';
   if(!!smallPadding) className += ' small-padding';
 
+  const style = ColorHelpers.generateStyleForColor('border', _.isString(borderTop) ? borderTop : 'main');
+
   return (
-    <table className={className}>
+    <table className={className} style={style}>
       {props.children}
     </table>
   );
