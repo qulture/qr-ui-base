32200080aa6b39244fd5f5d6b5b066a311c34da9
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:28:03 +0100

    Tab using custom color

--- components/tab/TabItem.jsx
+++ components/tab/TabItem.jsx
@@ -1,12 +1,14 @@
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 
 function TabItem(props) {
   const { children, active, link } = props;
   let className = 'qr-tab-item';
   if(!!active) className += ' active';
+  const activeStyle = ColorHelpers.generateStyleForColor(['border', 'color'], 'main');
 
   if(!!link) return <a className={className} href={link}>{children}</a>;
-  return <div className={className}>{children}</div>;
+  return <div style={activeStyle} className={className}>{children}</div>;
 }
 
 TabItem.propTypes = {
