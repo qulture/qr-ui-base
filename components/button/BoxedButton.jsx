3df1328c645e26bc8b0a51a969c4856b9086f719
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:01:34 +0100

    BoxedButton using custom color

--- components/button/BoxedButton.jsx
+++ components/button/BoxedButton.jsx
@@ -1,12 +1,14 @@
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 import IconLink from './../IconLink';
 import { Boxed } from './../boxed';
 
 function BoxedButton(props) {
   const { children, color, borderColor, icon, link, action, label } = props;
+  const style = ColorHelpers.generateStyleForColor('border', borderColor);
 
   return (
-    <Boxed color={borderColor}>
+    <Boxed color={borderColor} style={style}>
       <IconLink type={icon} link={link} action={action} size="large" color={color}>
         {children}
       </IconLink>
