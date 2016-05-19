1630d7399938c22a24ed57e91fc08a06d8f2809b
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:01:39 +0100

    Button using custom color

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,13 +1,15 @@
 import _ from 'lodash';
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 
 function Button(props) {
   const { children, action, link, color, fixedWidth } = props;
   const fixedWidthClass = _.isBoolean(fixedWidth) && fixedWidth ? 'fixed-width' : `fixed-width-${fixedWidth}`;
   const className = `qr-button ${color}-border-color, ${color}-color ${fixedWidthClass}`;
+  const style = ColorHelpers.generateStyleForColor(['border', 'color'], color);
 
   return (
-    <a className={className} onClick={action} href={link}>
+    <a className={className} onClick={action} href={link} style={style}>
       {children}
     </a>
   );
