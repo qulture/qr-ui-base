8c317e3b35621f9db43113c085041b5228d03cbc
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:01:15 +0100

    Icon using custom color

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,10 +1,14 @@
 import React, { Component, PropTypes } from 'react';
+import { ColorHelpers } from './../helpers';
 
 function Icon(props) {
   const { type, size, link, color, action } = props;
   let className = `icon-${type} ${size} ${color}-color ${props.className}`;
   if(!!link || !!action) className += ' clicable';
-  let iconComponent = !!link || !!action ? <a href={link} onClick={action} className={className}></a> : <span className={className}></span>;
+  const style = ColorHelpers.generateStyleForColor('color', color);
+
+  const commonProps = { className, style };
+  const iconComponent = !!link || !!action ? <a href={link} onClick={action} {...commonProps}></a> : <span {...commonProps}></span>;
 
   return iconComponent;
 }
