3895ff9da9ac397b8615d20db2b03255e556eb81
Christian Storch <ccstorch@gmail.com>
2016-05-19 13:49:28 +0100

    WizardItem using custom color

--- components/wizard/WizardItem.jsx
+++ components/wizard/WizardItem.jsx
@@ -1,13 +1,19 @@
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 
 function WizardItem(props) {
   const { children, completed, current, link, action } = props
   let className = 'qr-wizard-item';
-  if(!!current) className += ' current';
   if(!!completed) className += ' completed';
 
+  let style = {};
+  if(!!current) {
+    className += ' current';
+    style = ColorHelpers.generateStyleForColor(['bg', 'border'], 'main');
+  }
+
   return (
-    <a href={link} onClick={action} className={className}>
+    <a href={link} onClick={action} className={className} style={style}>
       {children}
     </a>
   );
