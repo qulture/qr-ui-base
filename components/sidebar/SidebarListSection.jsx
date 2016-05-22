bbf4cc4138127c398886012b0cfe6ce5769d6921
Christian Storch <ccstorch@gmail.com>
2016-05-22 18:50:17 +0100

    Hide h4 tag if no title is provided on SidebarListSection

--- components/sidebar/SidebarListSection.jsx
+++ components/sidebar/SidebarListSection.jsx
@@ -2,12 +2,11 @@ import React, { PropTypes, Component } from 'react';
 
 function SidebarListSection(props) {
   const { children, title } = props;
+  const titleDOMItem = !!title ? <h4 className="qr-sidebar-list-section-title detail-3-color">{title}</h4> : false;
 
   return (
     <section className="qr-sidebar-list-section">
-      <h4 className="qr-sidebar-list-section-title detail-3-color">
-        {title}
-      </h4>
+      {titleDOMItem}
       <div className="qr-sidebar-list-section-content">
         {children}
       </div>
