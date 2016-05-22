8d89f6da383835329b855ced43340e711b4dabf0
Christian Storch <ccstorch@gmail.com>
2016-05-22 18:48:32 +0100

    Separator with no padding options

--- components/Separator.jsx
+++ components/Separator.jsx
@@ -2,14 +2,16 @@ import React, { PropTypes } from 'react';
 import { ColorHelpers } from './../helpers';
 
 function Separator(props) {
-  const { size, withLine, lineWidth, lineStyle, lineColor } = props;
+  const { size, noPaddingBottom, noPaddingTop, withLine, lineWidth, lineStyle, lineColor } = props;
 
   const style = ColorHelpers.generateStyleForColor('border', lineColor);
   style.borderWidth = lineWidth;
   const lineClassName = `qr-separator-line ${lineStyle}-border ${lineColor}-border-color`;
   const separatorDOMItem = (!!withLine || !!lineStyle || !!lineWidth || !!lineColor) ? <div className={lineClassName} style={style}></div> : false;
 
-  const className = `qr-separator ${size}`;
+  let className = `qr-separator ${size}`;
+  if(!!noPaddingTop) className += ' no-padding-top';
+  if(!!noPaddingBottom) className += ' no-padding-bottom';
 
   return (
     <div className={className}>
@@ -20,6 +22,8 @@ function Separator(props) {
 
 Separator.propTypes = {
   size: PropTypes.string,
+  noPaddingTop: PropTypes.bool,
+  noPaddingBottom: PropTypes.bool,
   withLine: PropTypes.bool,
   lineWidth: PropTypes.number,
   lineStyle: PropTypes.string,
