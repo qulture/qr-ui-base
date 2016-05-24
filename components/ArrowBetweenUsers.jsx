7f4b14533da9e77caefd0e1587950b1b7a86e3cc
Christian Storch <ccstorch@gmail.com>
2016-05-25 00:27:10 +0100

    ArrowBetweenUsers with avatar size option

--- components/ArrowBetweenUsers.jsx
+++ components/ArrowBetweenUsers.jsx
@@ -2,12 +2,12 @@ import React, { PropTypes } from 'react';
 import { Avatar } from 'meteor/qulture:qr-ui-base/components';
 
 function ArrowBetweenUsers(props) {
-  const { leftUser, rightUser, label, sublabel } = props;
+  const { leftUser, rightUser, label, sublabel, size } = props;
 
   return (
     <div className="qr-arrow-wrapper">
       <div className="qr-arrow-left-content">
-        <Avatar user={leftUser} />
+        <Avatar user={leftUser} size={size} />
       </div>
 
       <div className="qr-arrow">
@@ -20,7 +20,7 @@ function ArrowBetweenUsers(props) {
       </div>
 
       <div className="qr-arrow-right-content">
-        <Avatar user={rightUser} />
+        <Avatar user={rightUser} size={size} />
       </div>
     </div>
   );
@@ -31,6 +31,7 @@ ArrowBetweenUsers.propTypes = {
   rightUser: PropTypes.object,
   label: PropTypes.string,
   sublabel: PropTypes.string,
+  size: PropTypes.string,
 };
 
 export default ArrowBetweenUsers;
