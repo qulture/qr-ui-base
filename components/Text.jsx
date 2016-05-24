de94c98fa5cf2c8562621c982bb4981668376769
Christian Storch <ccstorch@gmail.com>
2016-05-24 23:29:08 +0100

    Text uppercase and size props

--- components/Text.jsx
+++ components/Text.jsx
@@ -2,21 +2,19 @@ import React, { PropTypes } from 'react';
 import { ColorHelpers } from './../helpers';
 
 function Text(props) {
-  let { children, color, className } = props;
-  className += ` ${color}-color`;
+  let { children, color, className, uppercase, size } = props;
+  className += ` ${color}-color ${size}-font-size`;
+  if(!!uppercase) className += ' uppercase';
   const style = ColorHelpers.generateStyleForColor('color', color);
 
-  const commonProps = {
-    className,
-    style
-  };
-
-  return <span {...commonProps}>{children}</span>;
+  return <span className={className} style={style}>{children}</span>;
 }
 
 Text.propTypes = {
   color: PropTypes.string,
-  className: PropTypes.string
+  className: PropTypes.string,
+  uppercase: PropTypes.bool,
+  size: PropTypes.string,
 };
 
 Text.defaultProps = {
