579803334c21742e404f717c4c69f811bd6d5fbc
Christian Storch <ccstorch@gmail.com>
2016-05-24 23:29:17 +0100

    Link component using Text components

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,27 +1,30 @@
 import React, { PropTypes } from 'react';
 import { ColorHelpers } from './../helpers';
+import Text from './Text';
 
 function Link(props) {
-  let { children, link, action, color, className, isSubmit } = props;
-  className += ` clicable ${color}-color`;
-  const style = ColorHelpers.generateStyleForColor('color', color);
+  let { children, link, action, color, className, isSubmit, size, uppercase } = props;
+  className += ` clicable`;
 
   const commonProps = {
     href: link,
     onClick: action,
-    className,
-    style
+    className
   };
 
-  if(isSubmit) return <button type="submit" {...commonProps}>{children}</button>;
-  return <a {...commonProps}>{children}</a>;
+  const childrenDOMItem = <Text {...props}>{children}</Text>;
+
+  if(isSubmit) return <button type="submit" {...commonProps}>{childrenDOMItem}</button>;
+  return <a {...commonProps}>{childrenDOMItem}</a>;
 }
 
 Link.propTypes = {
   color: PropTypes.string,
   link: PropTypes.string,
   action: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
+  uppercase: PropTypes.bool,
+  size: PropTypes.string,
 };
 
 Link.defaultProps = {
