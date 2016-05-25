bdc9721a5d6130cc1b74a14d5ba1c0c225ccd918
Christian Storch <ccstorch@gmail.com>
2016-05-25 17:34:16 +0100

    Text component with wrapper and weight

--- components/Text.jsx
+++ components/Text.jsx
@@ -2,12 +2,15 @@ import React, { PropTypes } from 'react';
 import { ColorHelpers } from './../helpers';
 
 function Text(props) {
-  let { children, color, className, uppercase, size } = props;
-  className += ` ${color}-color ${size}-font-size`;
+  let { children, color, className, uppercase, size, weight, wrapper } = props;
+  className += ` ${color}-color ${size}-font-size ${weight}-weight`;
   if(!!uppercase) className += ' uppercase';
   const style = ColorHelpers.generateStyleForColor('color', color);
+  const commonProps = { className, style };
 
-  return <span className={className} style={style}>{children}</span>;
+  if(wrapper === 'p') return <p {...commonProps}>{children}</p>;
+  if(wrapper === 'div') return <div {...commonProps}>{children}</div>;
+  return <span {...commonProps}>{children}</span>;
 }
 
 Text.propTypes = {
@@ -15,6 +18,8 @@ Text.propTypes = {
   className: PropTypes.string,
   uppercase: PropTypes.bool,
   size: PropTypes.string,
+  wrapper: PropTypes.string,
+  weight: PropTypes.string,
 };
 
 Text.defaultProps = {
