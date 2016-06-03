e13260f67164a74ec73c1014fb1857aacc3db972
Christian Storch <ccstorch@gmail.com>
2016-06-04 00:53:25 +0100

    PanelHeader with grid option

--- components/panel/PanelHeader.jsx
+++ components/panel/PanelHeader.jsx
@@ -1,13 +1,19 @@
 import React, { PropTypes } from 'react';
 
 function PanelHeader(props) {
-  const { children } = props;
+  const { children, grid } = props;
+  let className = 'qr-panel-header';
+  if(!!grid) className += ' qr-grid';
 
   return (
-    <div className="qr-panel-header qr-grid">
+    <div className={className}>
       {children}
     </div>
   );
+}
+
+PanelHeader.propTypes = {
+  grid: PropTypes.bool
 }
 
 export default PanelHeader;
