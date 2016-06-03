078ed894037f038c181d8b310e1d73d48a90c482
Christian Storch <ccstorch@gmail.com>
2016-06-03 16:14:13 +0100

    contentColor option for Badge

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -2,11 +2,11 @@ import React, { Component, PropTypes } from 'react';
 import { ColorHelpers } from './../helpers';
 
 function Badge(props) {
-  const { children, color, content, textColor, size } = props;
+  const { children, color, content, textColor, contentColor, size } = props;
   const wrapperClassName = `qr-badge ${textColor}-color`;
-  const badgeClassName = `qr-badge-content ${color}-bg-color ${size}`;
+  const badgeClassName = `qr-badge-content ${color}-bg-color ${contentColor}-color ${size}`;
   const wrapperStyle = ColorHelpers.generateStyleForColor('color', textColor);
-  const badgeStyle = ColorHelpers.generateStyleForColor('bg', color);
+  const badgeStyle = _.merge(ColorHelpers.generateStyleForColor('bg', color), ColorHelpers.generateStyleForColor('color', contentColor));
 
   return (
     <div className={wrapperClassName} style={wrapperStyle}>
@@ -18,6 +18,7 @@ function Badge(props) {
 
 Badge.propTypes = {
   textColor: PropTypes.string,
+  contentColor: PropTypes.string,
   content: PropTypes.node,
   color: PropTypes.string,
   size: PropTypes.string,
@@ -25,6 +26,7 @@ Badge.propTypes = {
 
 Badge.defaultProps = {
   color: 'main',
+  contentColor: 'white'
 };
 
 export default Badge;
