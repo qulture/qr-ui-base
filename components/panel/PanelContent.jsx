0f8f59edc54ed1743ed85f8a231508114a727ae1
Christian Storch <ccstorch@gmail.com>
2016-06-04 21:24:34 +0100

    PanelContent with withLeftSpace props

--- components/panel/PanelContent.jsx
+++ components/panel/PanelContent.jsx
@@ -1,13 +1,19 @@
 import React, { PropTypes } from 'react';
 
 function PanelContent(props) {
-  const { children } = props;
+  const { children, withLeftSpace } = props;
+  let className = 'qr-panel-content';
+  if(!!withLeftSpace) className += ' with-left-space';
 
   return (
-    <div className="qr-panel-content">
+    <div className={className}>
       {children}
     </div>
   );
+}
+
+PanelContent.propTypes = {
+  withLeftSpace: PropTypes.bool
 }
 
 export default PanelContent;
