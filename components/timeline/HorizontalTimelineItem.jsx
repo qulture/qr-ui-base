671c3a2024a4f8898c2fcb1c316c116ea75d880a
Christian Storch <ccstorch@gmail.com>
2016-06-06 12:00:49 +0100

    HorizontalTimelineItem component

---
+++ components/timeline/HorizontalTimelineItem.jsx
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+
+function HorizontalTimelineItem(props) {
+  const { children, active, link, action, inactive } = props;
+  let className = 'qr-period-item';
+  if(active) className += ' active';
+  if(inactive) className += ' inactive';
+
+  return (
+    <a onClick={action} link={link} className={className}>
+      {children}
+    </a>
+  );
+}
+
+HorizontalTimelineItem.propTypes = {
+  active: PropTypes.bool,
+  inactive: PropTypes.bool,
+  link: PropTypes.string,
+  action: PropTypes.func,
+};
+
+export default HorizontalTimelineItem;
---
+++ components/timeline/index.js
@@ -0,0 +1,9 @@
+import HorizontalTimeline from './HorizontalTimeline';
+import HorizontalTimelineItem from './HorizontalTimelineItem';
+import PeriodsBar from './PeriodsBar';
+
+export {
+  HorizontalTimeline,
+  HorizontalTimelineItem,
+  PeriodsBar
+};
