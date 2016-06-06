8ecd48a959690188e9ff391ab671e1aca10a5708
Christian Storch <ccstorch@gmail.com>
2016-06-06 01:10:12 +0100

    largetMinPadding props for Boxed component

--- components/boxed/Boxed.jsx
+++ components/boxed/Boxed.jsx
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react';
 import { ColorHelpers } from './../../helpers';
 
 function Boxed(props) {
-  const { children, color, noPadding, rightContent, leftContent, dashed } = props;
+  const { children, color, noPadding, rightContent, leftContent, dashed, largerMinPadding } = props;
   let className=`qr-boxed-wrapper ${color}-border-color`;
   if(!!noPadding) className += ' no-padding';
   if(!!dashed) className += ' dashed-border';
   if(!!rightContent) className += ' larger-padding-right';
+  if(!!largerMinPadding) className += ' larger-min-padding-top-bottom';
   const rightContentDOMItem = !!rightContent ? <div className="right-content">{rightContent}</div> : '';
   const leftContentDOMItem = !!leftContent ? <div className="left-content">{leftContent}</div> : '';
   const style = ColorHelpers.generateStyleForColor('border', color);
@@ -25,7 +26,8 @@ Boxed.propTypes = {
   noPadding: PropTypes.bool,
   rightContent: PropTypes.node,
   leftContent: PropTypes.node,
-  dashed: PropTypes.bool
+  dashed: PropTypes.bool,
+  largerMinPadding: PropTypes.bool
 };
 
 Boxed.defaultProps = {
