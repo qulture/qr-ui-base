38361f205c30400fc5670c636177332c4f7df2c9
Christian Storch <ccstorch@gmail.com>
2016-06-09 22:14:21 +0100

    TableCell with main color

--- components/table/TableCell.jsx
+++ components/table/TableCell.jsx
@@ -1,13 +1,15 @@
 import React, { PropTypes } from 'react';
+import { ColorHelpers } from './../../helpers';
 import Link from './../Link';
 
 function TableCell(props) {
   let { children, className, link, color, cols, rows, action } = props;
   const contentDOMItem = !!link || !!action ? <Link link={link} action={action} className="selector" color={color}>{children}</Link> : children;
   className += ` ${color}-color`;
+  const style = ColorHelpers.generateStyleForColor('color', color);
 
   return (
-    <td className={className} rowSpan={rows} colSpan={cols}>
+    <td className={className} rowSpan={rows} colSpan={cols} style={style}>
       {contentDOMItem}
     </td>
   );
