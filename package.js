a749bdeb7849d6a8c3c62536b4c312a21e016b1f
Christian Storch <ccstorch@gmail.com>
2016-06-09 22:14:28 +0100

    0.2.2 version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'qulture:qr-ui-base',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'QR base react components and styles',
   git: 'https://github.com/qulture/qr-ui-base',
   documentation: 'README.md'
