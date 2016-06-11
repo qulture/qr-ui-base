a2b085c8331b5f98b3d8c912143e1e38d8cc3a82
Christian Storch <ccstorch@gmail.com>
2016-06-11 20:07:14 +0100

    ListModalItem with noPadding and lineColor props

--- components/modal/ListModalItem.jsx
+++ components/modal/ListModalItem.jsx
@@ -2,8 +2,10 @@ import React, { PropTypes, Component } from 'react';
 import Link from './../Link';
 
 function ListModalItem(props) {
-  const { children, link, action } = props;
-  const className = 'qr-list-modal-item';
+  const { children, link, action, noPaddingTop, noPaddingBottom, lineColor } = props;
+  let className = `qr-list-modal-item ${lineColor}-border-color`;
+  if(!!noPaddingTop) className += ' no-padding-top';
+  if(!!noPaddingBottom) className += ' no-padding-bottom';
 
   if(!!link || !!action) return <Link className={className} action={action} link={link}>{children}</Link>;
   return <div className={className}>{children}</div>;
@@ -11,7 +13,10 @@ function ListModalItem(props) {
 
 ListModalItem.propTypes = {
   action: PropTypes.func,
-  link: PropTypes.func
+  link: PropTypes.func,
+  noPaddingTop: PropTypes.bool,
+  noPaddingBottom: PropTypes.bool,
+  lineColor: PropTypes.string
 };
 
 export default ListModalItem;
