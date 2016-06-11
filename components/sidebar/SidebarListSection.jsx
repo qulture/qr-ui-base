37c5a2cbec2e4b8f2236087e92603a9fbc8a02ce
Christian Storch <ccstorch@gmail.com>
2016-06-11 20:07:42 +0100

    SidebarListSection with sidecontent

--- components/sidebar/SidebarListSection.jsx
+++ components/sidebar/SidebarListSection.jsx
@@ -1,12 +1,16 @@
 import React, { PropTypes, Component } from 'react';
+import { Grid, GridItem } from './../grid';
 
 function SidebarListSection(props) {
-  const { children, title } = props;
+  const { children, title, sideContent } = props;
   const titleDOMItem = !!title ? <h4 className="qr-sidebar-list-section-title detail-3-color">{title}</h4> : false;
 
   return (
     <section className="qr-sidebar-list-section">
-      {titleDOMItem}
+      <Grid align="center">
+        {titleDOMItem}
+        <div className="medium-padding-right">{sideContent}</div>
+      </Grid>
       <div className="qr-sidebar-list-section-content">
         {children}
       </div>
@@ -16,6 +20,7 @@ function SidebarListSection(props) {
 
 SidebarListSection.propTypes = {
   title: PropTypes.string,
+  sideContent: PropTypes.node
 };
 
 export default SidebarListSection;
